9dff6aef5b8535a2bcebd858cdd75b47931d1c9e
ghigolini <45860903+ghigolini@users.noreply.github.com>
2022-04-22 16:10:15 +0200

    Add files via upload

--- js/er_faina.js
+++ js/er_faina.js
@@ -1,3 +1,16 @@
-function myFunction() {
-    alert("Hello! I am an alert box!");
-  }
+var canvas = document.getElementById("mycanvas");
+var ctx = canvas.getContext("2d");
+
+class Player{
+    constructor(xpos, ypos){
+        this.xpos = xpos;
+        this.ypos = ypos;
+    }
+
+}
+
+function draw(){
+    
+}
+
+setInterval(draw, 10);
